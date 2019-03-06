31f2f8a01bb65f35b5d976abd3625f8dcc0395ec
Jonas friedli <jonas.friedli@ch.ibm.com>
2019-03-06 14:14:18 +0100

    play with bean scopes

---
+++ src/main/java/ch/jfriedli/springdemo/springdemoone/BeanScopeDemoApp.java
@@ -0,0 +1,26 @@
+package ch.jfriedli.springdemo.springdemoone;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		
+		//load spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		
+		//retrieve bean from spring container
+		Coach coach = context.getBean("myCoach", Coach.class);
+		
+		Coach alphaCoach = context.getBean("myCoach", Coach.class);
+		
+		//check if they are the sam
+		boolean result = (coach.equals(alphaCoach));
+		
+		System.out.println("It is '" + result + "' that the objects are the same!");
+		System.out.println("Memory location for coach: " + coach);
+		System.out.println("Memory location for alphaCoach: " + alphaCoach);
+		
+	}
+
+}
